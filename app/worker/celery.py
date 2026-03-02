3a1e567adb23be95f8355cb632e89a5fba09f128
only-dpp <richardvlogs78@gmail.com>
2026-03-02 20:16:35 -0300

    update in worker

--- app/worker/celery.py
+++ app/worker/celery.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from celery import Celery
 from app.core.config import settings
 
@@ -7,14 +6,4 @@ celery = Celery(
     broker=settings.REDIS_URL,
     backend=settings.REDIS_URL,
 )
-=======
-from celery import Celery
-from app.core.config import settings
-
-celery = Celery(
-    "coreops",
-    broker=settings.REDIS_URL,
-    backend=settings.REDIS_URL,
-)
->>>>>>> 21599b0b39eba37876fc43d9838497fbe2974000
 celery.conf.task_track_started = True
